ea6772e9be9f5f5d84b52870f01ee6a927c7f9e2
v_congxwen <v_congxwen@tencent.com>
2022-03-06 18:35:52 +0800

    feat: 安装html-webpack-plugin的插件，将打包的js插入到html中运行

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 安装webpack： npm install webpack webpack-cli --save-dev --registry=https://registry.npmmirror.com 后添加的--registry=https://registry.npmmirror.com是taobao镜像使下载更快 安装成功后，新建webpack.dev.config.js文件，进行模态的书写配置 在package.js中添加："dev": "webpack --config ./webpack.dev.config.js --mode development" 命令行中
 
-安装插件： npm install webpack-dev-serve html-webback-plugin --save-dev
+2-2安装html-webpack-plugin的插件，将打包的js插入到html的模板中，并自动更新执行
+安装插件：npm install webpack-dev-server html-webpack-plugin --save-dev
+
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname,
         filename: './release/bundle.js'
+    },
+    plugins: [
+        // 将./src/index.js插入到html的模板中
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, './release'), // 根目录；课程中的contentBase更改为static
+        open: true, // 自动打开浏览器
+        port: 9000,
     }
 }
